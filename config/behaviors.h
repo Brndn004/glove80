84666e3c55623e10b68749afd847ca27e92d8706
Brandon Draper <brandon@applied.co>
2024-05-21 19:59:41 -0700

    Add vim behavior

--- config/behaviors.h
+++ config/behaviors.h
@@ -25,21 +25,17 @@
             tapping-term-ms = <100>;
             bindings = <&mo_sticky LAYER_Hold LAYER_OneShot>, <&to LAYER_Hold>;
         };
-        //   Tap once: param
-        //  Tap twice: vim_leader param
-        vim_dance: vim_dance {
-            compatible = "zmk,behavior-tap-dance";
-            #binding-cells = <2>;
-            tapping-term-ms = <100>;
-            bindings = <&kp>, <&kp_vim>;
-        };
-        //   Tap once: param
-        //  Tap twice: vim_leader param
-        vim_dance_double: vim_dance_double {
+        vim_w: vim_s {
             compatible = "zmk,behavior-tap-dance";
             #binding-cells = <0>;
             tapping-term-ms = <100>;
-            bindings = <&kp>, <&kp_vim>, <&kp_vim_double>;
+            bindings = <&kp S>, <&kp_vim SQT>, <&kp_vim DQT>;
+        };
+        vim_w: vim_w {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <100>;
+            bindings = <&kp W>, <&kp_vim W>, <&kp_vim_double W Q>;
         };
     };
 };
