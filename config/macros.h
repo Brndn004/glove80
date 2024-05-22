6134d61e3ee1797539d7d0e2c755f0b44eaaec47
Brandon Draper <brandon@applied.co>
2024-05-21 18:42:57 -0700

    Add macros, don't use them

--- config/macros.h
+++ config/macros.h
@@ -37,7 +37,7 @@
                 , <&kp R>
                 , <&kp S>;
         };
-    adp_start_docker_example: adp_start_docker_example {
+    asde: adp_start_docker_example {
             compatible = "zmk,behavior-macro";
             #binding-cells = <0>;
             tap-ms = <1>;
@@ -68,7 +68,7 @@
                 , <&kp L>
                 , <&kp E>;
         };
-    adp_run_example_optimized: adp_run_example_optimized {
+    areo: adp_run_example_optimized {
             compatible = "zmk,behavior-macro";
             #binding-cells = <0>;
             tap-ms = <1>;
@@ -111,7 +111,29 @@
                 = <&sk LGUI>
                 , <&kp L>;
         };
-    desktop_up: desktop_up {
+    slack_prev: slack_prev {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&kt LALT>
+                , <&sk LSHFT>
+                , <&kp UP>
+                , <&kt LALT>;
+        };
+    slack_next: slack_next {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&kt LALT>
+                , <&sk LSHFT>
+                , <&kp DOWN>
+                , <&kt LALT>;
+        };
+    dt_up: desktop_up {
             compatible = "zmk,behavior-macro";
             #binding-cells = <0>;
             tap-ms = <1>;
@@ -122,7 +144,7 @@
                 , <&kp UP>
                 , <&kt LCTRL>;
         };
-    desktop_down: desktop_down {
+    dt_dn: desktop_down {
             compatible = "zmk,behavior-macro";
             #binding-cells = <0>;
             tap-ms = <1>;
@@ -133,7 +155,7 @@
                 , <&kp DOWN>
                 , <&kt LCTRL>;
         };
-    open_terminal: open_terminal {
+    term_open: open_terminal {
             compatible = "zmk,behavior-macro";
             #binding-cells = <0>;
             tap-ms = <1>;
@@ -186,7 +208,62 @@
                 , <&kp PG_UP>
                 , <&kt LCTRL>;
         };
-    window_next: window_next {
+    win_snap_left: window_snap_left {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&sk LGUI>
+                , <&kp LEFT>;
+        };
+    win_snap_right: window_snap_right {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&sk LGUI>
+                , <&kp RIGHT>;
+        };
+    win_up: window_up {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&kt LCTRL>
+                , <&kt LALT>
+                , <&sk LSHFT>
+                , <&kp UP>
+                , <&kt LALT>
+                , <&kt LCTRL>;
+        };
+    win_dn: window_dn {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&kt LCTRL>
+                , <&kt LALT>
+                , <&sk LSHFT>
+                , <&kp DOWN>
+                , <&kt LALT>
+                , <&kt LCTRL>;
+        };
+    win_prev: window_prev {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&kt LALT>
+                , <&sk LSHFT>
+                , <&kp TAB>
+                , <&kt LALT>;
+        };
+    win_next: window_next {
             compatible = "zmk,behavior-macro";
             #binding-cells = <0>;
             tap-ms = <1>;
@@ -195,7 +272,7 @@
                 = <&sk LALT>
                 , <&kp TAB>;
         };
-    window_close: window_close {
+    win_close: window_close {
             compatible = "zmk,behavior-macro";
             #binding-cells = <0>;
             tap-ms = <1>;
@@ -222,17 +299,47 @@
                 = <&sk LCTRL>
                 , <&kp MINUS>;
         };
-    // 1 param macro
-    one_param: one_param {
+    hist_back: hist_back {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&sk LALT>
+                , <&kp LEFT>;
+        };
+    hist_for: hist_for {
+            compatible = "zmk,behavior-macro";
+            #binding-cells = <0>;
+            tap-ms = <1>;
+            wait-ms = <1>;
+            bindings
+                = <&sk LALT>
+                , <&kp RIGHT>;
+        };
+    // 1 param macros.
+    // Type the given key preceded by vim leader.
+    kp_vim: kp_vim {
         compatible = "zmk,behavior-macro-one-param";
         #binding-cells = <1>;
         tap-ms = <1>;
         wait-ms = <1>;
         bindings
-            = <&macro_press &kp LSHFT>
+            = <&kp VIM_LEADER>
+            , <&macro_param_1to1>
+            , <&kp MACRO_PLACEHOLDER>;
+        };
+    kp_vim_double: kp_vim_double {
+        compatible = "zmk,behavior-macro-one-param";
+        #binding-cells = <1>;
+        tap-ms = <1>;
+        wait-ms = <1>;
+        bindings
+            = <&kp VIM_LEADER>
             , <&macro_param_1to1>
             , <&kp MACRO_PLACEHOLDER>
-            , <&macro_release &kp LSHFT>;
+            , <&macro_param_1to1>
+            , <&kp MACRO_PLACEHOLDER>;
         };
     };
 };
