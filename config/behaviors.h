cbb3952ac9adf405c2cc6b3e692cfec93c3afeb7
Brandon Draper <brandon@applied.co>
2024-05-21 20:47:05 -0700

    All vim cmds; LAYER_Base complete minus thumbs

--- config/behaviors.h
+++ config/behaviors.h
@@ -18,6 +18,18 @@
             tapping-term-ms = <100>;
             bindings = <&mo_sticky LAYER_Hold LAYER_OneShot>, <&to LAYER_Hold>;
         };
+        vim_b: vim_b {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp B>, <&kp_vim B>;
+        };
+        vim_c: vim_c {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp C>, <&kp_vim Q>, <&kp_vim_double Q Q>;
+        };
         vim_d: vim_d {
             compatible = "zmk,behavior-tap-dance";
             #binding-cells = <0>;
@@ -29,6 +41,30 @@
             #binding-cells = <0>;
             tapping-term-ms = <200>;
             bindings = <&kp E>, <&kp_vim E>;
+        };
+        vim_f: vim_f {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp F>, <&kp_vim F>;
+        };
+        vim_g: vim_g {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp G>, <&kp_vim N5>;
+        };
+        vim_h: vim_h {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp H>, <&kp_vim N6>;
+        };
+        vim_i: vim_i {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp I>, <&kp_vim STAR>;
         };
         vim_r: vim_r {
             compatible = "zmk,behavior-tap-dance";
@@ -48,11 +84,29 @@
             tapping-term-ms = <300>;
             bindings = <&kp T>, <&kp_vim T>;
         };
+        vim_u: vim_u {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp U>, <&kp_vim HASH>;
+        };
         vim_w: vim_w {
             compatible = "zmk,behavior-tap-dance";
             #binding-cells = <0>;
             tapping-term-ms = <200>;
             bindings = <&kp W>, <&kp_vim W>, <&kp_vim_double W Q>;
+        };
+        vim_x: vim_x {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp X>, <&kp_vim X>;
+        };
+        vim_z: vim_z {
+            compatible = "zmk,behavior-tap-dance";
+            #binding-cells = <0>;
+            tapping-term-ms = <200>;
+            bindings = <&kp Z>, <&kp_vim Z>;
         };
     };
 };
